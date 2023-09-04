a358358e324657b3dc965745b15e28c81047fad1
Jeethanxx01 <jeethancs@gmail.com>
2023-09-04 20:50:23 +0530

    updated

---
+++ questions3.js
@@ -0,0 +1,77 @@
+// creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+     numb: 1,
+      question: "What is the main function used for in C programming?",
+      answer: "It is the entry point of a C program",
+      options: [
+        "It is used for mathematical operations",
+        "It is used to declare variables",
+        "It is used to print output",
+        "It is the entry point of a C program",
+      ]
+    },
+      {
+      numb: 2,
+      question: "What is the purpose of the 'printf' function in C?",
+      answer: "It is used to print formatted output on the console",
+      options: [
+        "It is used to read input from the user",
+        "It is used for conditional statements",
+        "It is used for looping",
+        "It is used to print formatted output on the console",
+      ]
+    },
+    {
+      numb: 3,
+      question: "What is a 'variable' in C programming?",
+      answer: "A named storage location that can hold data",
+      options: [
+        "A conditional statement",
+        "A function",
+        "A named storage location that can hold data",
+        "A loop construct",
+      ]
+    },
+      {
+       numb: 4,
+      question: "What is the purpose of the 'if' statement in C?",
+      answer: "To make decisions based on conditions",
+      options: [
+        "To declare variables",
+        "To print output",
+        "To perform arithmetic operations",
+        "To make decisions based on conditions",
+       
+      ]
+    },
+    {
+      numb: 5,
+      question: "What is the 'data type' of the variable 'x' in the declaration 'int x;'?",
+      answer: "Integer",
+      options: [
+        "Float",
+        "Character",
+        "String",
+        "Integer",
+       
+      ]
+    },
+    {
+      numb: 6,
+      numb: 6,
+      question: "What is a 'pointer' in C programming?",
+      answer: "A variable that stores the memory address of another variable",
+      options: [
+        "A type of loop",
+        "A way to print output",
+        "A variable that stores the memory address of another variable",
+        "A type of data structure",
+       
+      ]
+    },
+    
+  
+  ];
+  
---
+++ questions4.js
@@ -0,0 +1,76 @@
+// creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+      numb: 1,
+      question: "What is a data structure?",
+      answer: "A way of organizing and storing data for efficient retrieval and manipulation",
+      options: [
+        "A computer program",
+        "A type of algorithm",
+        "A way of organizing and storing data for efficient retrieval and manipulation",
+        "A mathematical equation",
+      ]
+    },
+      {
+     numb: 2,
+      question: "What is an algorithm?",
+      answer: "A step-by-step procedure for solving a problem",
+      options: [
+        "A data structure",
+        "A computer program",
+        "A step-by-step procedure for solving a problem",
+        "A type of variable",
+      ]
+    },
+    {
+       numb: 3,
+      question: "What is the time complexity of an algorithm?",
+      answer: "A measure of the amount of time an algorithm takes to run as a function of the input size",
+      options: [
+        "The number of steps in an algorithm",
+        "The number of variables in a program",
+        "A measure of the amount of time an algorithm takes to run as a function of the input size",
+        "The space complexity of an algorithm",
+      ]
+    },
+      {
+      numb: 4,
+      question: "What is a linked list in data structures?",
+      answer: "A linear data structure in which elements are linked using pointers",
+      options: [
+        "A way to sort data",
+        "A type of tree structure",
+        "A linear data structure in which elements are linked using pointers",
+        "A type of algorithm",
+       
+      ]
+    },
+    {
+       numb: 5,
+      question: "What is the purpose of a stack in data structures?",
+      answer: "To store and manage data in a last-in, first-out (LIFO) manner",
+      options: [
+        "To store and manage data in a first-in, first-out (FIFO) manner",
+        "To perform mathematical calculations",
+        "To store and manage data in a last-in, first-out (LIFO) manner",
+        "To represent hierarchical data",
+       
+      ]
+    },
+    {
+     numb: 6,
+      question: "What is the difference between a binary search tree and a binary tree?",
+      answer: "A binary search tree has a specific ordering property, while a binary tree does not.",
+      options: [
+        "A binary search tree always has two children for each node, while a binary tree may have any number of children.",
+        "A binary search tree has a specific ordering property, while a binary tree does not.",
+        "A binary tree is always balanced, while a binary search tree is not.",
+        "There is no difference; both terms refer to the same thing.",
+       
+      ]
+    },
+    
+  
+  ];
+  
